3b70bd64de17ffc2bcc18cb67eaebce8adb24eb0
Simske <mail@simske.com>
2023-06-25 00:01:14 +0200

    feat(imports): manage top-level package exports

--- pyiof/__init__.py
+++ pyiof/__init__.py
@@ -1,1 +1,18 @@
-__version__ = "0.1.0"
+import importlib.metadata
+
+__version__ = importlib.metadata.version(__package__ or __name__)
+
+del importlib.metadata
+
+from .message_elements import (
+    ClassList,
+    CompetitorList,
+    ControlCardList,
+    CourseData,
+    EntryList,
+    EventList,
+    OrganisationList,
+    ResultList,
+    ServiceRequestList,
+    StartList,
+)
