abe0e6f9c5a54c5288179dd9cf35be7c7494fbb2
Simske <mail@simske.com>
2025-01-31 12:53:35 +0100

    chore(lint): switch to pyright for type checking

--- pyiof/xml_base.py
+++ pyiof/xml_base.py
@@ -1,3 +1,5 @@
+from typing import Self, cast
+
 import pydantic_xml
 from pydantic_xml import attr, element  # noqa: F401
 
@@ -12,20 +14,23 @@ class BaseXmlModel(  # type: ignore
     def to_xml_tree(self, skip_empty: bool = True, **kwargs):
         return super().to_xml_tree(skip_empty=skip_empty, **kwargs)
 
-    def to_xml(self, pretty_print: bool = True, **kwargs):
-        return super().to_xml(
-            pretty_print=pretty_print,
-            xml_declaration=True,
-            encoding="UTF-8",
-            skip_empty=True,
-            **kwargs,
+    def to_xml(self, pretty_print: bool = True, **kwargs) -> bytes:
+        return cast(
+            bytes,
+            super().to_xml(
+                pretty_print=pretty_print,
+                xml_declaration=True,
+                encoding="UTF-8",
+                skip_empty=True,
+                **kwargs,
+            ),
         )
 
     @classmethod
-    def read_xml(cls, path: str, encoding: str = "utf-8"):
+    def read_xml(cls, path: str, encoding: str = "utf-8") -> Self:
         with open(path, "rb") as f:
             return cls.from_xml(f.read())
 
-    def write_xml(self, path: str):
+    def write_xml(self, path: str) -> None:
         with open(path, "wb") as f:
             f.write(self.to_xml())
